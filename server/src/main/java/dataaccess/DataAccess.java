0963969de4095dc8073f8e8f3b9892f63bc9b4b5
preland <prelandofficialmusic@gmail.com>
2025-07-16 17:02:21 -0600

    impl of dataaccess stubs

--- server/src/main/java/dataaccess/DataAccess.java
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -1,29 +1,48 @@
 package dataaccess;
 
+import model.AuthData;
 import model.UserData;
 import org.eclipse.jetty.server.Authentication;
 
 public class DataAccess {
+    private MemoryStore storage;
     private static final DataAccess instance = new DataAccess();
-    private DataAccess() {}
+    private DataAccess() {
+        this.storage = MemoryStore.getInstance();
+        //above should be trivially switchable from memory to sql
+    }
     public static DataAccess getInstance() {return instance;}
     public void clear() {
         //get static reference to memorystore and use it to run function
-        MemoryStore.getInstance().clear();
+        storage.clear();
     }
 
     public UserData getUser(String username, String password) {
         //todo:actually implement
-        return new UserData(username, password, "testos");
+        //return new UserData(username, password, "testos");
+        return storage.getUser(username, password);
+
     }
 
     public UserData createUser(String username, String password, String email) {
         //todo:actually implement
-        return new UserData(username, password, email);
+        //return new UserData(username, password, email);
+        return storage.createUser(username, password, email);
     }
 
-    public String createAuth(UserData userData) {
+    public AuthData createAuth(UserData userData) {
         //todo:actually implement
-        return "{}";
+        //return "{}";
+        String username = userData.username();
+        String password = userData.password();
+        return storage.createAuth(username, password);
+    }
+
+    public boolean deleteAuth(String authToken) {
+        return storage.deleteAuth(authToken);
+    }
+
+    public String getUsername(String authToken) {
+        return storage.getUsername(authToken);
     }
 }
--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -3,6 +3,7 @@ package service;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import dataaccess.DataAccess;
+import model.AuthData;
 import model.GameData;
 import model.UserData;
 
@@ -19,7 +20,7 @@ public class Service {
         //call dataaccess thingy to delete everything
         dataAccess.clear();
     }
-    public String register(String username, String password, String email) throws ServiceException {
+    public AuthData register(String username, String password, String email) throws ServiceException {
         if(dataAccess.getUser(username, password) != null) {
             throw new ServiceException("username already taken");
         }
@@ -28,7 +29,7 @@ public class Service {
         return dataAccess.createAuth(userData);
     }
 
-    public String login(String username, String password) throws ServiceException {
+    public AuthData login(String username, String password) throws ServiceException {
         UserData userData = dataAccess.getUser(username, password);
         if(userData == null) {
             throw new ServiceException("user doesn't exist");
