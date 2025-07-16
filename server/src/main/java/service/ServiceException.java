6f3c3addf0d548444d3d807f87f974602c57c79d
preland <prelandofficialmusic@gmail.com>
2025-07-16 17:58:48 -0600

    update exception init

--- server/src/main/java/service/ServiceException.java
+++ server/src/main/java/service/ServiceException.java
@@ -1,8 +1,27 @@
 package service;
 
 public class ServiceException extends Exception {
-    public ServiceException(String message) {
-        super(message);
+    private final int errorCode;
+    private final String message;
+    public ServiceException(int errorCode) {
+        String message = "";
+        switch(errorCode) {
+            case 400:
+                message = "{ \"message\": \"Error: bad request\" }";
+                break;
+            case 401:
+                message = "{ \"message\": \"Error: unauthorized\" }";
+            case 403:
+                message = "{ \"message\": \"Error: already taken\" }";
+                break;
+            default:
+                message = "{ \"message\": \"Error: unknown\" }";
+                break;
+        }
+        //super(message);
+        this.errorCode = errorCode;
+        this.message = message;
     }
-    public ServiceException(String message, Throwable ex) { super(message, ex); }
+
+    //public ServiceException(String message, Throwable ex) { super(message, ex); }
 }
