ef8178b686b191fd16044a360d5b3300ed0bf75f
preland <prelandofficialmusic@gmail.com>
2025-07-16 17:14:16 -0600

    impl getuser and createuser memory functionality

--- server/src/main/java/dataaccess/MemoryStore.java
+++ server/src/main/java/dataaccess/MemoryStore.java
@@ -5,6 +5,7 @@ import model.GameData;
 import model.UserData;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MemoryStore {
     private static final MemoryStore instance = new MemoryStore();
@@ -17,5 +18,24 @@ public class MemoryStore {
         userData.clear();
         authData.clear();
         gameData.clear();
+    }
+
+    public UserData getUser(String username, String password) {
+        UserData testData = userData.stream().filter(ud -> username.equals(ud.username())).findFirst().orElse(null);
+        if(testData == null) {
+            return null;
+        }
+        //todo: implement non-plaintext password stuff
+        if(Objects.equals(testData.password(), password)) {
+            return testData;
+        } else {
+            return null;
+        }
+    }
+
+    public UserData createUser(String username, String password, String email) {
+        UserData newUser = new UserData(username, password, email);
+        userData.add(newUser);
+        return newUser;
     }
 }
