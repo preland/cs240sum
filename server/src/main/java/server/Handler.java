db5e63ce8535b16ff85f6aa483b9e9e57e3e762f
preland <prelandofficialmusic@gmail.com>
2025-07-16 18:06:59 -0600

    gson null pointer handling

--- server/src/main/java/server/Handler.java
+++ server/src/main/java/server/Handler.java
@@ -21,6 +21,7 @@ public class Handler {
         service.clear();
     }
     public String register(String body) throws ServiceException {
+        try {
         var serializer = new Gson();
         JsonObject jsonObject = serializer.fromJson(body, JsonObject.class);
         String username = jsonObject.get("username").getAsString();
@@ -28,46 +29,69 @@ public class Handler {
         String email = jsonObject.get("email").getAsString();
 
         return serializer.toJson(service.register(username, password, email));
+        } catch (NullPointerException e) {
+            throw new ServiceException(400);
+        }
     }
 
     public String login(String body) throws ServiceException {
+        try {
         var serializer = new Gson();
         JsonObject jsonObject = serializer.fromJson(body, JsonObject.class);
         String username = jsonObject.get("username").getAsString();
         String password = jsonObject.get("password").getAsString();
 
         return serializer.toJson(service.login(username, password));
+        } catch (NullPointerException e) {
+            throw new ServiceException(400);
+        }
     }
 
     public void logout(String body) throws ServiceException {
+        try {
         var serializer = new Gson();
         JsonObject jsonObject = serializer.fromJson(body, JsonObject.class);
         String authToken = jsonObject.get("authToken").getAsString();
         service.logout(authToken);
+        } catch (NullPointerException e) {
+            throw new ServiceException(400);
+        }
     }
 
     public ArrayList<GameData> listGames(String body) throws ServiceException {
+        try {
         var serializer = new Gson();
         JsonObject jsonObject = serializer.fromJson(body, JsonObject.class);
         String authToken = jsonObject.get("authToken").getAsString();
         return service.listGames(authToken);
+        } catch (NullPointerException e) {
+            throw new ServiceException(400);
+        }
     }
 
     public int createGame(String body) throws ServiceException {
+        try {
         var serializer = new Gson();
         JsonObject jsonObject = serializer.fromJson(body, JsonObject.class);
         String authToken = jsonObject.get("authToken").getAsString();
         String gameName = jsonObject.get("gameName").getAsString();
         return service.createGame(authToken, gameName);
+        } catch (NullPointerException e) {
+            throw new ServiceException(400);
+        }
     }
 
     public void joinGame(String body) throws ServiceException {
+        try {
         var serializer = new Gson();
         JsonObject jsonObject = serializer.fromJson(body, JsonObject.class);
         String authToken = jsonObject.get("authToken").getAsString();
         String playerColor = jsonObject.get("playerColor").getAsString();
         int gameID = jsonObject.get("gameID").getAsInt();
         service.joinGame(authToken, playerColor, gameID);
+        } catch (NullPointerException e) {
+            throw new ServiceException(400);
+        }
     }
 }
 
