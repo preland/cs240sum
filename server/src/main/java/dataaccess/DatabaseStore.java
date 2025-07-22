49ebd1676ba0e40c68ca9473590845b9e619ff69
preland <prelandofficialmusic@gmail.com>
2025-07-22 11:28:45 -0600

    add sql scaffolding

--- server/src/main/java/dataaccess/DatabaseStore.java
+++ server/src/main/java/dataaccess/DatabaseStore.java
@@ -5,6 +5,7 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.UUID;
@@ -18,16 +19,38 @@ public class DatabaseStore {
     private DatabaseStore() {}
     public static DatabaseStore getInstance() { return INSTANCE; }
     public void clear() {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
+
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 
     public UserData getUser(String username, String password) {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
+
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 
     public UserData createUser(String username, String password, String email) {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
 
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
     public AuthData createAuth(String username, String password) {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
 
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 
     private String generateToken() {
@@ -35,24 +58,56 @@ public class DatabaseStore {
     }
 
     public boolean deleteAuth(String authToken) {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
+
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 
     public String getUsername(String authToken) {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
 
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 
     public ArrayList<GameData> listGames() {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
 
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 
     public GameData createGame(String gameName) {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
 
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 
     public GameData getGame(int gameID) {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
 
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 
     public void modifyGame(int gameID, String username, String playerColor) {
+        var statement = "do stuff here";
+        try(var conn = DatabaseManager.getConnection()) {
+
+        } catch(DataAccessException | SQLException e) {
+            //do something here!
+        }
     }
 }
