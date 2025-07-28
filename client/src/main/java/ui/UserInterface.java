44edc33f0cfc20a70ea12d2556aa70dd6688ca83
preland <prelandofficialmusic@gmail.com>
2025-07-27 23:22:52 -0600

    init impl of prelogin login and register ui

--- client/src/main/java/ui/UserInterface.java
+++ client/src/main/java/ui/UserInterface.java
@@ -4,12 +4,13 @@ public class UserInterface {
     boolean quit = false;
     boolean postLogin = false;
     public void run() {
-        String input = "";
+        String rawInput = "";
         System.out.println("welcome chess. type help for help. type quit for unhelp");
         while (!quit) {
-            input = System.in.toString();
+            rawInput = System.in.toString();
+            String[] input = rawInput.split(" ");
             if (postLogin) {
-                switch (input) {
+                switch (input[0]) {
                     case "help":
                         System.out.println("lol no help for you, sucks ig");
                         break;
@@ -18,13 +19,27 @@ public class UserInterface {
                         System.out.println("invalid input. try again, or type help");
                 }
             } else {
-                switch (input) {
+                switch (input[0]) {
                     case "help":
                         System.out.println("lol no help for you, sucks ig");
                         break;
                     case "quit":
                         quit = true;
                         System.out.println("goodb");
+                        break;
+                    case "login":
+                        if(input.length != 3) {
+                            System.out.println("invalid input. try again, or type help");
+                            break;
+                        }
+                        handleLogin(input[1], input[2]);
+                        break;
+                    case "register":
+                        if(input.length != 4) {
+                            System.out.println("invalid input. try again, or type help");
+                            break;
+                        }
+                        handleRegister(input[1], input[2], input[3]);
                         break;
                     default:
                         System.out.println("invalid input. try again, or type help");
