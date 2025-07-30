74abc47ab78d9a098ce8aafcca2ccb9f7bdd16c0
preland <prelandofficialmusic@gmail.com>
2025-07-29 19:46:26 -0600

    add unit testing

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,16 +2,21 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
+
+import java.util.ArrayList;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
-
+    private static ServerFacade facade;
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
+        facade = new ServerFacade(port);
+        facade.clear();
         System.out.println("Started test HTTP server on " + port);
     }
 
@@ -20,10 +25,78 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
+    @BeforeEach
+    public void first() {
+        facade.clear();
+    }
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
     }
 
+    @Test
+    void loginPositive() {
+        facade.register("username", "password", "email");
+        Assertions.assertDoesNotThrow(() -> facade.login("username", "password"));
+    }
+    @Test
+    void loginNegative() {
+        Assertions.assertEquals("autherror",facade.login("username", "password"));
+    }
+
+    @Test
+    void registerPositive() {
+        Assertions.assertNotEquals("takenerror", facade.register("username", "password", "email"));
+    }
+    @Test
+    void registerNegative() {
+        facade.register("username", "password", "email");
+        Assertions.assertEquals("takenerror",facade.register("username", "password", "email"));
+    }
+
+    @Test
+    void logoutPositive() {
+        String token = facade.register("username", "password", "email");
+        Assertions.assertNotEquals("connerror", facade.logout(token));
+    }
+    @Test
+    void logoutNegative() {
+        Assertions.assertEquals("", facade.logout("faketoken"));
+    }
+
+    @Test
+    void joinGamePositive() {
+        String token = facade.register("username", "password", "email");
+        String id = facade.createGame("anme", token);
+        Assertions.assertNotEquals("connerror", facade.joinGame(id.substring(0,1), true, token));
+    }
+    @Test
+    void joinGameNegative() {
+        String token = facade.register("username", "password", "email");
+        Assertions.assertEquals("takenerror", facade.joinGame("10", true, token));
+    }
+
+    @Test
+    void listGamesPositive() {
+        String token = facade.register("username", "password", "email");
+        String id = facade.createGame("anme", token);
+        Assertions.assertNotNull(facade.listGames(token));
+    }
+    @Test
+    void listGamesNegative() {
+        String token = facade.register("username", "password", "email");
+        Assertions.assertEquals(new ArrayList<>(), facade.listGames(token));
+    }
+
+    @Test
+    void createGamePositive() {
+        String token = facade.register("username", "password", "email");
+        Assertions.assertEquals("1.0", facade.createGame("name", token));
+    }
+    @Test
+    void createGameNegative() {
+        String token = facade.register("username", "password", "email");
+        String id = facade.createGame("anme", token);
+        Assertions.assertNotEquals("1.0", facade.createGame("name", token));
+    }
 }
